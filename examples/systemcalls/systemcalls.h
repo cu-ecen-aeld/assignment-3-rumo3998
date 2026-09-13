954d34e404210f9ea317fd84f39e8e38cb55b17e
Ruben Moreno <rumo3998@colorado.edu>
2026-09-13 02:54:49 -0700

    Implemented the do_system(), do_exec(), and the do_exec_redirect() function calls, also updated the .h files to include all necessary libraries and headers

--- examples/systemcalls/systemcalls.h
+++ examples/systemcalls/systemcalls.h
@@ -1,9 +1,17 @@
+/*Import libraries for input, output, booleans, and facilities to step through args
+ * for variadic functions.*/
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdarg.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <fcntl.h>
 
 bool do_system(const char *command);
 
 bool do_exec(int count, ...);
 
 bool do_exec_redirect(const char *outputfile, int count, ...);
+
+
--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -49,8 +49,9 @@ then
 	fi
 fi
 echo "Removing the old writer utility and compiling as a native application"
-make clean
-make
+#Removing the make step as a x-compile make step will be added in a different script.
+#make clean
+#make
 
 for i in $( seq 1 $NUMFILES)
 do
